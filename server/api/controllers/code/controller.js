32e04f4aae3cb4b7e6baf9127d08872c96cb51d4
Tushar3099 <tusharneogi30@gmail.com>
2020-12-31 16:22:45 +0530

    containerizing using docker

--- server/api/controllers/code/controller.js
+++ server/api/controllers/code/controller.js
@@ -4,16 +4,33 @@ import CodeService from "../../services/code.service";
 export class Controller {
   async execute(req, res) {
     try {
-      const { code, input } = req.body;
+      const { code, input, id } = req.body;
       const { lang } = req.query;
-      const output = await CodeService.execute(code, input, lang);
-      if (output) {
-        res.send({
-          status: "200",
-          message: "Code Successfully Executed",
-          output,
-        });
+      if (code && lang) {
+        const output = await CodeService.execute(code, input, lang, id);
+        if (output) {
+          res.send({
+            status: "200",
+            message: "Code Successfully Executed",
+            output,
+          });
+        }
+      } else {
+        throw { message: "Invalid Input" };
       }
+    } catch (error) {
+      res.send({
+        status: error.status || "500",
+        message: error.message || "Something Went Wrong",
+      });
+    }
+  }
+  async getcwd(req, res) {
+    try {
+      res.send({
+        status: "200",
+        message: "Current working directory is : " + process.cwd(),
+      });
     } catch (error) {
       res.send({
         status: error.status || "500",
