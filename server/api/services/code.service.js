447bfe1082d017e0bb6356c79ceffc6a1429977d
Tushar3099 <tusharneogi30@gmail.com>
2021-01-03 00:27:07 +0530

    minor fixes

--- server/api/services/code.service.js
+++ server/api/services/code.service.js
@@ -1,7 +1,8 @@
 import fs from "fs";
+import path from "path";
 import { execFile, spawn, exec } from "child_process";
-const ROOT_DIR = "/app";
-const SOURCE_DIR = `${ROOT_DIR}/executor`;
+const ROOT_DIR = `${process.cwd()}`;
+const SOURCE_DIR = path.join(ROOT_DIR, "executor");
 const TARGET_DIR = `/app/codes`;
 const IMAGE_NAME = "executor:1.1";
 
@@ -15,7 +16,6 @@ class CodeService {
       const { runCode, runContainer } = await this.writeCommand(
         lang,
         file,
-        // outputFile,
         inputFile,
         id
       );
@@ -31,7 +31,6 @@ class CodeService {
       );
 
       if (OUTPUT) {
-        // await this.deleteFiles(fileName, inputName, lang);
         return OUTPUT.toString();
       }
     } catch (error) {
@@ -58,19 +57,15 @@ class CodeService {
         throw { message: "Invalid language" };
       }
     }
-    fs.writeFile(`${SOURCE_DIR}/${fileName}`, code, (err) => {
+    fs.writeFile(path.join(SOURCE_DIR, fileName), code, (err) => {
       if (err) throw { message: err };
     });
-    // fs.writeFile(`${SOURCE_DIR}/${id}output.txt`, "", (err) => {
-    //   if (err) throw { message: err };
-    // });
-    fs.writeFile(`${SOURCE_DIR}/${id}input.txt`, input, (err) => {
+    fs.writeFile(path.join(SOURCE_DIR, `${id}input.txt`), input, (err) => {
       if (err) throw { message: err };
     });
 
     return {
       file: fileName,
-      // output: `${id}output.txt`,
       inputFile: `${id}input.txt`,
     };
   }
@@ -119,8 +114,6 @@ class CodeService {
           if (stderr) {
             reject({ message: stderr });
           } else {
-            // const outputData = fs.createReadStream(`${SOURCE_DIR}/${outputFile}`);
-            // resolve(outputData.toString());
             resolve(stdout);
           }
         });
@@ -129,16 +122,16 @@ class CodeService {
   }
 
   async deleteFiles(fileName, inputName, lang, id) {
-    fs.unlinkSync(`${SOURCE_DIR}/${fileName}`, (err) => {
+    fs.unlinkSync(path.join(SOURCE_DIR, fileName), (err) => {
       if (err) throw err;
     });
     if (inputName) {
-      fs.unlinkSync(`${SOURCE_DIR}/${inputName}`, (err) => {
+      fs.unlinkSync(path.join(SOURCE_DIR, inputName), (err) => {
         if (err) throw err;
       });
     }
     if (lang == "c++") {
-      fs.unlinkSync(`${SOURCE_DIR}/${id}`, (err) => {
+      fs.unlinkSync(path.join(SOURCE_DIR, id), (err) => {
         if (err) throw err;
       });
     }
