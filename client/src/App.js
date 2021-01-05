06d20341a609773537b2e0773e3526fadd6b37c3
tomarviii88 <tomarviii88@gmail.com>
2021-01-05 15:34:01 +0530

    theme-switching enabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './constants/theme.js';
+import { GlobalStyles } from './constants/global.js';
+import { useDarkMode } from './utils/useDarkMode';
+import Header from './components/header/Header.js';
 
 const App = () => {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />;
+  }
   return (
-    <div>
-      <h1>Remote Code Executor</h1>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <div>
+        <h1>Remote Code Executor</h1>
+      </div>
+    </ThemeProvider>
   );
 };
 
