58797c75e9c572cf8bb5c4392a35d73a764bceb8
Tushar3099 <tusharneogi30@gmail.com>
2021-01-11 23:18:10 +0530

    adding draggrable screen

--- client/src/components/code-editor/Editor.js
+++ client/src/components/code-editor/Editor.js
@@ -1,14 +1,14 @@
-import React, { useRef, useState } from 'react';
-import Editor from '@monaco-editor/react';
-import { executeCode, setLoadingTrue } from '../../actions/code';
-import { useDispatch, useSelector } from 'react-redux';
-import Input from './Input';
-import Submit from './Submit';
-import Split from 'react-split';
-import styled from 'styled-components';
-import styles from './styles/editor.module.css';
+import React, { useRef, useState } from "react";
+import Editor from "@monaco-editor/react";
+import { executeCode, setLoadingTrue } from "../../actions/code";
+import { useDispatch, useSelector } from "react-redux";
+import Input from "./Input";
+import Submit from "./Submit";
+import Split from "react-split";
+import styled from "styled-components";
+import styles from "./styles/editor.module.css";
 
-import { Play } from 'react-feather';
+import { Play } from "react-feather";
 
 const Row = styled.div`
   display: flex;
@@ -20,20 +20,20 @@ const OutputWindow = styled.div`
   border-radius: 5px;
   padding: 0 20px;
   box-sizing: border-box;
-  color: ${props => (props.error ? 'red' : 'black')};
+  color: ${(props) => (props.error ? "red" : "black")};
 `;
 
 const CodeEditor = ({ theme }) => {
-  const loading = useSelector(state => state.code.isFetching);
+  const loading = useSelector((state) => state.code.isFetching);
   const [isEditorReady, setIsEditorReady] = useState(false);
-  const [language, setLanguage] = useState('c');
-  const [input, setInput] = useState('');
+  const [language, setLanguage] = useState("c");
+  const [input, setInput] = useState("");
   const dispatch = useDispatch();
   const valueGetter = useRef();
-  let output = useSelector(state => state.code.output);
-  let error = useSelector(state => state.code.error);
+  let output = useSelector((state) => state.code.output);
+  let error = useSelector((state) => state.code.error);
 
-  const handleEditorDidMount = _valueGetter => {
+  const handleEditorDidMount = (_valueGetter) => {
     setIsEditorReady(true);
     valueGetter.current = _valueGetter;
   };
@@ -45,48 +45,64 @@ const CodeEditor = ({ theme }) => {
     dispatch(res);
   };
 
-  const changeLanguage = e => {
+  const changeLanguage = (e) => {
     setLanguage(e.target.value);
   };
 
   return (
     <>
       <div className={styles.row}>
-        <div className={styles.left}>
-          <div className={styles.runCode}>
-            <Submit language={language} changeLanguage={changeLanguage} />
-            <div
-              className={styles.submitButton}
-              onClick={SubmitCode}
-              disabled={!isEditorReady}
-            >
-              {loading ? 'Loading..' : 'Run Code'}
-              <Play style={{ paddingLeft: 10, fontSize: '1em' }} />
+        <Split
+          direction="horizontal"
+          sizes={[50, 50]}
+          minSize={0}
+          snapOffset={200}
+          className={styles.splitHor}
+        >
+          <div className={styles.left}>
+            <div className={styles.runCode}>
+              <Submit language={language} changeLanguage={changeLanguage} />
+              <div
+                className={styles.submitButton}
+                onClick={SubmitCode}
+                disabled={!isEditorReady}
+              >
+                {loading ? "Loading.." : "Run Code"}
+                <Play style={{ paddingLeft: 10, fontSize: "1em" }} />
+              </div>
+            </div>
+            <Editor
+              wrapperClassName="editor"
+              language={language}
+              theme={theme === "dark" ? "vs-dark" : "light"}
+              editorDidMount={handleEditorDidMount}
+            />
+          </div>
+          <div className={styles.right}>
+            <div className={styles.column}>
+              <Split
+                direction="vertical"
+                sizes={[50, 50]}
+                minSize={0}
+                snapOffset={200}
+                className={styles.splitVer}
+              >
+                <div className={styles.output}>
+                  <div className={styles.outputHead}>Output</div>
+                  <OutputWindow error={error === "" ? false : true}>
+                    {output ? console.log(output) : null}
+                    <pre style={{ width: "100%" }}>
+                      {output === "" ? error : output}
+                    </pre>
+                  </OutputWindow>
+                </div>
+                <div className={styles.input}>
+                  <Input input={input} setInput={setInput} theme={theme} />
+                </div>
+              </Split>
             </div>
           </div>
-          <Editor
-            wrapperClassName='editor'
-            language={language}
-            theme={theme === 'dark' ? 'vs-dark' : 'light'}
-            editorDidMount={handleEditorDidMount}
-          />
-        </div>
-        <div className={styles.right}>
-          <div className={styles.column}>
-            <div className={styles.output}>
-              <div className={styles.outputHead}>Output</div>
-              <OutputWindow error={error === '' ? false : true}>
-                {output ? console.log(output) : null}
-                <pre style={{ width: '100%' }}>
-                  {output === '' ? error : output}
-                </pre>
-              </OutputWindow>
-            </div>
-            <div className={styles.input}>
-              <Input input={input} setInput={setInput} theme={theme} />
-            </div>
-          </div>
-        </div>
+        </Split>
       </div>
     </>
   );
