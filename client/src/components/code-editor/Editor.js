6860d2bd8bf5c284b815af2ff3d35faf8bd76eb0
Tushar3099 <tusharneogi30@gmail.com>
2021-01-13 22:10:35 +0530

    adding responsiveness

--- client/src/components/code-editor/Editor.js
+++ client/src/components/code-editor/Editor.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import { executeCode, setLoadingTrue } from "../../actions/code";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,7 +7,7 @@ import Submit from "./Submit";
 import Split from "react-split";
 import styled from "styled-components";
 import styles from "./styles/editor.module.css";
-
+import "./styles/style.css";
 import { Play } from "react-feather";
 
 const Row = styled.div`
@@ -18,20 +18,36 @@ const Row = styled.div`
 
 const OutputWindow = styled.div`
   border-radius: 5px;
-  padding: 0 20px;
+  padding: 20px;
   box-sizing: border-box;
+  overflow: auto;
+  flex: 1;
   color: ${(props) => (props.error ? "red" : "black")};
 `;
 
 const CodeEditor = ({ theme }) => {
   const loading = useSelector((state) => state.code.isFetching);
   const [isEditorReady, setIsEditorReady] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
   const [language, setLanguage] = useState("c");
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
   const valueGetter = useRef();
   let output = useSelector((state) => state.code.output);
   let error = useSelector((state) => state.code.error);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
+  }, []);
+
+  const updateWindowDimensions = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+  };
 
   const handleEditorDidMount = (_valueGetter) => {
     setIsEditorReady(true);
@@ -48,15 +64,16 @@ const CodeEditor = ({ theme }) => {
   const changeLanguage = (e) => {
     setLanguage(e.target.value);
   };
-
   return (
     <>
       <div className={styles.row}>
         <Split
-          direction="horizontal"
-          sizes={[50, 50]}
-          minSize={0}
-          snapOffset={200}
+          direction={windowWidth > 800 ? "horizontal" : "vertical"}
+          sizes={[60, 40]}
+          minSize={500}
+          // snapOffset={200}
+          gutterSize={20}
+          gutterAlign="center"
           className={styles.splitHor}
         >
           <div className={styles.left}>
@@ -72,7 +89,8 @@ const CodeEditor = ({ theme }) => {
               </div>
             </div>
             <Editor
-              wrapperClassName="editor"
+              // wrapperClassName="editor"
+              className="editor"
               language={language}
               theme={theme === "dark" ? "vs-dark" : "light"}
               editorDidMount={handleEditorDidMount}
@@ -82,9 +100,11 @@ const CodeEditor = ({ theme }) => {
             <div className={styles.column}>
               <Split
                 direction="vertical"
-                sizes={[50, 50]}
+                sizes={windowWidth > 800 ? [75, 25] : [100, 0]}
                 minSize={0}
-                snapOffset={200}
+                // snapOffset={100}
+                gutterSize={20}
+                gutterAlign="center"
                 className={styles.splitVer}
               >
                 <div className={styles.output}>
